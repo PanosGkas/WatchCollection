9683125dc578e6530bf822b8f9647dc735b6322d
PanosGkas <panosgassios@hotmail.com>
2025-08-21 20:56:26 +0300

    image problem v2

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base: "/WatchCollection",
+      base: "/WatchCollection/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
